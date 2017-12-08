57ac4b92dd2a06295f1e445e7e3105091cc8058e
Geoffrey <hogsleigh@gmail.com>
2017-12-08 09:04:38 +0300

    Update WelcomeController.java

--- src/main/java/com/in28minutes/login/WelcomeController.java
+++ src/main/java/com/in28minutes/login/WelcomeController.java
@@ -4,14 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WelcomeController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String showLoginPage(ModelMap model) {
-		model.put("name", "in28Minutes");
-		return "welcome";
+	@ResponseBody
+	public String showLoginPage(@RequestParam String meter) {
+		return "This is the java end point for a generic service";
 	}
 
 }
